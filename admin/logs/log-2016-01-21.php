b6ad1b6959d8522fad137461731f11f065952654
PoppyLi <83613578@qq.com>
2016-01-21 19:05:47 +0800

    finished

---
+++ admin/logs/log-2016-01-21.php
@@ -0,0 +1,8 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2016-01-21 19:01:56 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away C:\wamp\www\PCMS\libs\ci\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2016-01-21 19:02:00 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away C:\wamp\www\PCMS\libs\ci\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2016-01-21 19:02:16 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away C:\wamp\www\PCMS\libs\ci\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2016-01-21 19:02:19 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away C:\wamp\www\PCMS\libs\ci\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2016-01-21 19:02:27 --> Severity: Notice  --> Undefined variable: parent_path C:\wamp\www\PCMS\admin\views\columns_edit.php 111
+ERROR - 2016-01-21 19:02:27 --> Severity: Warning  --> Invalid argument supplied for foreach() C:\wamp\www\PCMS\admin\views\columns_edit.php 111
---
+++ site/logs/log-2016-01-21.php
@@ -0,0 +1,41 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2016-01-21 19:01:24 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:01:24 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away C:\wamp\www\PCMS\libs\ci\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2016-01-21 19:01:30 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:01:30 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away C:\wamp\www\PCMS\libs\ci\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2016-01-21 19:01:30 --> Severity: Notice  --> Undefined variable: content C:\wamp\www\PCMS\site\views\cases.php 10
+ERROR - 2016-01-21 19:01:30 --> Severity: Notice  --> Undefined variable: content C:\wamp\www\PCMS\site\views\cases.php 10
+ERROR - 2016-01-21 19:01:31 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:01:31 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away C:\wamp\www\PCMS\libs\ci\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2016-01-21 19:01:31 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:01:32 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:01:32 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:01:33 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:01:34 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:01:34 --> Severity: Notice  --> Undefined variable: content C:\wamp\www\PCMS\site\views\feedback.php 10
+ERROR - 2016-01-21 19:01:34 --> Severity: Notice  --> Undefined variable: content C:\wamp\www\PCMS\site\views\feedback.php 10
+ERROR - 2016-01-21 19:01:34 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:01:36 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:01:37 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:01:37 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:02:52 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:02:54 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:00 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:03 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:05 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:32 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:33 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:33 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:34 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:34 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:34 --> Severity: Notice  --> Undefined variable: content C:\wamp\www\PCMS\site\views\cases.php 10
+ERROR - 2016-01-21 19:04:34 --> Severity: Notice  --> Undefined variable: content C:\wamp\www\PCMS\site\views\cases.php 10
+ERROR - 2016-01-21 19:04:34 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:34 --> Severity: Notice  --> Undefined variable: content C:\wamp\www\PCMS\site\views\decoration.php 10
+ERROR - 2016-01-21 19:04:34 --> Severity: Notice  --> Undefined variable: content C:\wamp\www\PCMS\site\views\decoration.php 10
+ERROR - 2016-01-21 19:04:35 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:35 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:43 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:44 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2016-01-21 19:04:44 --> Language file contains no data: language/zh_CN/common_lang.php
